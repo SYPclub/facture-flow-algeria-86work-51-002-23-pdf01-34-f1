a0d6e4175ec25a4cfbb09b8b15014c06497b1aac
hjuuuuuyybe <khomri12@gmail.com>
2025-05-27 20:44:16 +0100

    Update FinalInvoiceDetail.tsx

--- src/pages/invoices/FinalInvoiceDetail.tsx
+++ src/pages/invoices/FinalInvoiceDetail.tsx
@@ -729,7 +729,7 @@ const FinalInvoiceDetail = () => {
                   <strong className="font-semibold">Mode de paiement:</strong>{" "}
                   
                     
-                    {paymentMethods[parseInt(invoice.paymentReference)] || 'inconnu'}
+                    {invoice.payment_type}
                     
                   
                 </div>
@@ -831,6 +831,16 @@ const FinalInvoiceDetail = () => {
                       {formatCurrency(invoice.taxTotal)}
                     </td>
                   </tr>
+                  {form.getValues('payment_type') === 'cash' && (
+                    <tr>
+                    <td colSpan={5} className="px-4 py-2 text-right font-semibold">
+                      droit de timbre:
+                    </td>
+                    <td colSpan={3} className="px-4 py-2 text-right">
+                      {formatCurrency(invoice.stampTax)}
+                    </td>
+                  </tr>
+                  )}
                   <tr className="border-t">
                     <td colSpan={5} className="px-4 py-2 text-right font-bold text-lg">
                       Total:
