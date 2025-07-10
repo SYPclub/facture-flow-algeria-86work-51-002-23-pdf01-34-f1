fcd40273325cdd037967d3b5eea3bdb1953e02d8
Startup program for youth (SPY) <khomri12@gmail.com>
2025-07-10 09:50:14 +0100

    .sort((a, b) => new Date(b.issuedate).getTime() - new Date(a.issuedate).getTime())

--- src/pages/invoices/FinalInvoicesPage.tsx
+++ src/pages/invoices/FinalInvoicesPage.tsx
@@ -96,7 +96,7 @@ const FinalInvoicesPage = () => {
 
   // Sort invoices
   const sortedInvoices = [...filteredInvoices]
-    .sort((a, b) => new Date(b.createdat) > new Date(a.createdat) ? 1 : -1);
+    .sort((a, b) => new Date(b.issuedate).getTime() - new Date(a.issuedate).getTime());
 
   // Calculate pagination
   const totalItems = sortedInvoices.length;
