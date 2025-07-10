5321abf8c6116b7cbf891cebbc0e6154fdb8545e
Startup program for youth (SPY) <khomri12@gmail.com>
2025-07-10 15:07:05 +0100

    defaultValue={field.value ?? 19}

--- src/pages/products/ProductDetail.tsx
+++ src/pages/products/ProductDetail.tsx
@@ -471,7 +471,7 @@ const ProductDetail = () => {
                     <FormItem>
                       <FormLabel>Taux d'imposition (%)</FormLabel>
                       <FormControl>
-                        <Input  type="number" {...field} />
+                        <Input  type="number"  {...field} defaultValue={field.value ?? 19} />
                       </FormControl>
                       <FormMessage />
                     </FormItem>
